e4199c91593d6089eb9b2a38e715ef7e73294667
Kushan-Nilanga <dknathalage@gmail.com>
2021-09-03 11:20:13 +1000

    scatter finished

--- addition/scatter.cpp
+++ addition/scatter.cpp
@@ -7,15 +7,11 @@
 using namespace std::chrono;
 using namespace std;
 
-void randomVector(int vector[], long int start, long int end, int batchsize)
+void randomVector(int vector[], long int end)
 {
-    bool skip = false;
-    for (int i = start; i < end; i++)
+    for (int i = 0; i < end; i++)
     {
-        if (skip == false)
-            vector[i] = rand() % 100;
-        if (i % batchsize == 0 && i != start)
-            skip = !skip;
+        vector[i] = rand() % 100;
     }
 }
 
@@ -34,38 +30,60 @@ int main(int argc, char **argv)
 
     unsigned long size = 100000000;
 
+    unsigned long batch_size = size / world_size;
+
     auto start = high_resolution_clock::now();
 
-    unsigned long batch_size = size / world_size;
-    int *v1 = (int *)malloc(2 * size * sizeof(int *));
-    int *v2 = (int *)malloc(size * sizeof(int *));
+    // allocating memory for the arrays
+    // v1 is the 1st outgoing array
+    int *v1;
 
+    // v2 is the 2nd outgoing array
+    int *v2;
+
+    // final array
+    int *v3;
+
+    // populating out array in head process
     if (rank == 0)
     {
-        randomVector(v1, 0, 2 * size, batch_size);
-        randomVector(v1, batch_size, 2 * size, batch_size);
+        // to prevent unnecessory allocations in nodes
+        v1 = (int *)malloc(size * sizeof(int *));
+        v2 = (int *)malloc(size * sizeof(int *));
+        v3 = (int *)malloc(size * sizeof(int *));
+
+        // v1 init
+        randomVector(v1, size);
+
+        // v2 init
+        randomVector(v2, size);
     }
 
-    int *data = (int *)malloc(2 * batch_size * sizeof(int *));
+    // Input of node
+    int *data1 = (int *)malloc(batch_size * sizeof(int *));
+    int *data2 = (int *)malloc(batch_size * sizeof(int *));
+    int *result = (int *)malloc(batch_size * sizeof(int *));
 
     // mpi scatter
-    MPI_Scatter(v1, 2 * size, MPI_INT, data, 2 * batch_size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(v1, batch_size, MPI_INT, data1, batch_size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    int *result = (int *)malloc(batch_size * sizeof(int *));
+    // mpi scatter
+    MPI_Scatter(v2, batch_size, MPI_INT, data2, batch_size, MPI_INT, 0, MPI_COMM_WORLD);
+
     for (long i = 0; i < batch_size; i++)
     {
-        result[i] = data[i] + data[batch_size + i];
+        result[i] = data1[i] + data2[i];
     }
 
-    // mpi gather
-    MPI_Gather(result, batch_size, MPI_INT, v2, size, MPI_INT, 0, MPI_COMM_WORLD);
+    // mpi gatheris one and send_datatype is MPI_INT
+    MPI_Gather(result, batch_size, MPI_INT, v3, batch_size, MPI_INT, 0, MPI_COMM_WORLD);
 
     auto stop = high_resolution_clock::now();
 
     auto duration = duration_cast<microseconds>(stop - start);
+    if (rank == 0)
+        cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
 
-    cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
     MPI_Finalize();
     return 0;
 }
